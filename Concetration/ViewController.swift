3d19688b2f457580539ce4d1f1ac317e62753399
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-04 19:34:00 +0400

    added cards  , and basic game logic

--- Concetration/ViewController.swift
+++ Concetration/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var touchButtons: [UIButton]!
+    
+    var emojis = ["👻","🎃","👻","🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let index = touchButtons.firstIndex(of: sender) {
+            flipEmoji(withEmoji: emojis[index], on: sender)
+        }
+    }
+    
+    func flipEmoji(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
-
