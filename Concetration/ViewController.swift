b4cd62a4d8a8519c12d68a5e437996ade5604b32
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-04 17:11:31 +0400

    Initial Commit

---
+++ Concetration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concetration
+//
+//  Created by Tigran on 1/4/21.
+//  Copyright © 2021 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
